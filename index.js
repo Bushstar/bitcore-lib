72f3150e4c6f878f4b7d8781435202ee0395dfc1
Peter Bushnell <bushsolo@gmail.com>
2018-06-07 07:36:40 +0000

    Convert to Feathercoin

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
 'use strict';
 
-var bitcore = module.exports;
+var feathercore = module.exports;
 
 // module information
-bitcore.version = 'v' + require('./package.json').version;
-bitcore.versionGuard = function(version) {
+feathercore.version = 'v' + require('./package.json').version;
+feathercore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of bitcore-lib found. ' +
-      'Please make sure to require bitcore-lib and check that submodules do' +
-      ' not also include their own bitcore-lib dependency.';
+    var message = 'More than one instance of feathercore-lib found. ' +
+      'Please make sure to require feathercore-lib and check that submodules do' +
+      ' not also include their own feathercore-lib dependency.';
     throw new Error(message);
   }
 };
-bitcore.versionGuard(global._bitcore);
-global._bitcore = bitcore.version;
+feathercore.versionGuard(global._feathercore);
+global._feathercore = feathercore.version;
 
 // crypto
-bitcore.crypto = {};
-bitcore.crypto.BN = require('./lib/crypto/bn');
-bitcore.crypto.ECDSA = require('./lib/crypto/ecdsa');
-bitcore.crypto.Hash = require('./lib/crypto/hash');
-bitcore.crypto.Random = require('./lib/crypto/random');
-bitcore.crypto.Point = require('./lib/crypto/point');
-bitcore.crypto.Signature = require('./lib/crypto/signature');
+feathercore.crypto = {};
+feathercore.crypto.BN = require('./lib/crypto/bn');
+feathercore.crypto.ECDSA = require('./lib/crypto/ecdsa');
+feathercore.crypto.Hash = require('./lib/crypto/hash');
+feathercore.crypto.Random = require('./lib/crypto/random');
+feathercore.crypto.Point = require('./lib/crypto/point');
+feathercore.crypto.Signature = require('./lib/crypto/signature');
 
 // encoding
-bitcore.encoding = {};
-bitcore.encoding.Base58 = require('./lib/encoding/base58');
-bitcore.encoding.Base58Check = require('./lib/encoding/base58check');
-bitcore.encoding.BufferReader = require('./lib/encoding/bufferreader');
-bitcore.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
-bitcore.encoding.Varint = require('./lib/encoding/varint');
+feathercore.encoding = {};
+feathercore.encoding.Base58 = require('./lib/encoding/base58');
+feathercore.encoding.Base58Check = require('./lib/encoding/base58check');
+feathercore.encoding.BufferReader = require('./lib/encoding/bufferreader');
+feathercore.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
+feathercore.encoding.Varint = require('./lib/encoding/varint');
 
 // utilities
-bitcore.util = {};
-bitcore.util.buffer = require('./lib/util/buffer');
-bitcore.util.js = require('./lib/util/js');
-bitcore.util.preconditions = require('./lib/util/preconditions');
+feathercore.util = {};
+feathercore.util.buffer = require('./lib/util/buffer');
+feathercore.util.js = require('./lib/util/js');
+feathercore.util.preconditions = require('./lib/util/preconditions');
 
 // errors thrown by the library
-bitcore.errors = require('./lib/errors');
+feathercore.errors = require('./lib/errors');
 
 // main bitcoin library
-bitcore.Address = require('./lib/address');
-bitcore.Block = require('./lib/block');
-bitcore.MerkleBlock = require('./lib/block/merkleblock');
-bitcore.BlockHeader = require('./lib/block/blockheader');
-bitcore.HDPrivateKey = require('./lib/hdprivatekey.js');
-bitcore.HDPublicKey = require('./lib/hdpublickey.js');
-bitcore.Networks = require('./lib/networks');
-bitcore.Opcode = require('./lib/opcode');
-bitcore.PrivateKey = require('./lib/privatekey');
-bitcore.PublicKey = require('./lib/publickey');
-bitcore.Script = require('./lib/script');
-bitcore.Transaction = require('./lib/transaction');
-bitcore.URI = require('./lib/uri');
-bitcore.Unit = require('./lib/unit');
+feathercore.Address = require('./lib/address');
+feathercore.Block = require('./lib/block');
+feathercore.MerkleBlock = require('./lib/block/merkleblock');
+feathercore.BlockHeader = require('./lib/block/blockheader');
+feathercore.HDPrivateKey = require('./lib/hdprivatekey.js');
+feathercore.HDPublicKey = require('./lib/hdpublickey.js');
+feathercore.Networks = require('./lib/networks');
+feathercore.Opcode = require('./lib/opcode');
+feathercore.PrivateKey = require('./lib/privatekey');
+feathercore.PublicKey = require('./lib/publickey');
+feathercore.Script = require('./lib/script');
+feathercore.Transaction = require('./lib/transaction');
+feathercore.URI = require('./lib/uri');
+feathercore.Unit = require('./lib/unit');
 
 // dependencies, subject to change
-bitcore.deps = {};
-bitcore.deps.bnjs = require('bn.js');
-bitcore.deps.bs58 = require('bs58');
-bitcore.deps.Buffer = Buffer;
-bitcore.deps.elliptic = require('elliptic');
-bitcore.deps._ = require('lodash');
+feathercore.deps = {};
+feathercore.deps.bnjs = require('bn.js');
+feathercore.deps.bs58 = require('bs58');
+feathercore.deps.Buffer = Buffer;
+feathercore.deps.elliptic = require('elliptic');
+feathercore.deps._ = require('lodash');
 
 // Internal usage, exposed for testing/advanced tweaking
-bitcore.Transaction.sighash = require('./lib/transaction/sighash');
+feathercore.Transaction.sighash = require('./lib/transaction/sighash');
